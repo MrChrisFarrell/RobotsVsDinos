3c2cc45cd09413779b0795d0f0c3b70965994bd9
unknown <mrchrisfarrell@yahoo.com>
2021-05-14 13:59:01 -0400

    added loops for incorrect inputs.

--- battlefield.py
+++ battlefield.py
@@ -20,21 +20,26 @@ class Battlefield:
         self.fleet.create_fleet()
         self.herd.create_herd()
         self.user_choice = input('Choose "dinos" or "robots"')
+        while not self.user_choice == 'dinos' or self.user_choice == 'robots':
+            self.user_choice = input('Incorrect Input: Choose "dinos" or "robots"')
         if self.user_choice == "dinos":
-            self.user = self.herd
-            self.enemy = self.fleet
+            self.user = self.herd.dinosaurs
+            self.enemy = self.fleet.robots
             print("\nYou're team of dinos consists of:")
-            for dino in self.user.dinosaurs:
+            for dino in self.user:
                 print(f'Type - {dino.type}, Energy - {dino.energy}, Power - {dino.attack_power},Health - {dino.health}')
             print("\nYou're enemy robots are:")
-            for robo in self.enemy.robots:
+            for robo in self.enemy:
                 print(f'Name - {robo.name}, Energy - {robo.power_level}, Health - {robo.health}, Weapon - {robo.weapon.type} (atk) {robo.weapon.attack_power}')
         else:
-            self.user = self.fleet
-            self.enemy = self.herd
-
-        self.battle(self.user, self.enemy)
-
+            self.user = self.fleet.robots
+            self.enemy = self.herd.dinosaurs
+            print("\nYou're team of robots consists of:")
+            for robo in self.user:
+                print(f'Name - {robo.name}, Energy - {robo.power_level}, Health - {robo.health}, Weapon - {robo.weapon.type} (atk) {robo.weapon.attack_power}')
+            print("\nYou're enemy dinosaurs are:")
+            for dino in self.enemy:
+                print(f'Type - {dino.type}, Energy - {dino.energy}, Power - {dino.attack_power},Health - {dino.health}')
 
     def display_welcome(self):
         print("Welcome to Robots vs Dinosaurs!")
